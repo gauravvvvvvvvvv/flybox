f2129b154e2e44249f3f4223fe420a99d4234a2b
Gaurav <109208028+gauravvvvvvvvvv@users.noreply.github.com>
2026-09-19 09:31:47 +0530

    Route docs and help paths to technical manual

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import DocsPage from "./DocsPage";
 import "./styles.css";
+
+const path = window.location.pathname.replace(/\/+$/, "") || "/";
+const Root = path === "/docs" || path === "/help" ? DocsPage : App;
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <Root />
   </React.StrictMode>
 );
